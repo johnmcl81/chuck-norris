f4c494e99cd24fb86ba8f224887ae39450bf5472
johnmcl81 <j.d.mclachlan@gmail.com>
2018-03-18 17:49:27 +0100

    I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+	#-----------------------------------------------------------------------------
+	def set_locale
+		if params[:lang]
+			I18n.locale = params[:lang]
+		else
+			I18n.locale = extract_browser_locale
+		end
+		@lang = I18n.locale
+  end
+  
+  private
+
+	#-----------------------------------------------------------------------------
+	def extract_browser_locale
+		if request.env['HTTP_ACCEPT_LANGUAGE'] && request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first == 'es'
+			'es'
+		else
+			'en'
+		end
+	end
 end
